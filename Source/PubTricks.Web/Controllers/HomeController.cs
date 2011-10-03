cc1e0717fe074d1bc3ae5df979926934e26ef02f
unknown <dave@.(none)>
2011-10-03 14:02:32 +1300

    got structure setup

--- Source/PubTricks.Web/Controllers/HomeController.cs
+++ Source/PubTricks.Web/Controllers/HomeController.cs
@@ -3,12 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PubTricks.Web.Infrastructure;
 
 namespace PubTricks.Web.Controllers {
     public class HomeController : Controller {
+        ILogger _logger;
+
+        //Every time you see a request for ILogger interface in a controller, return a new NLogger class
+        public HomeController(ILogger logger) {
+            _logger = logger;
+        }
+
         public ActionResult Index() {
             ViewBag.Message = "Welcome to ASP.NET MVC!";
-
+            _logger.LogInfo("In home");
             return View();
         }
 
